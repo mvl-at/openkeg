475ccd35cf1152ef88290f86a4bcb3c393ea8f49
Richard Stöckl <richard.stoeckl@aon.at>
2022-08-15 20:03:18 +0200

    Refactor the configuration of the build state

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,6 @@ extern crate rocket;
 use std::env;
 use std::sync::Arc;
 
-use figment::Figment;
 use ldap3::tokio::task;
 use okapi::merge::merge_specs;
 use okapi::openapi3::OpenApi;
@@ -57,6 +56,7 @@ mod user;
 pub type MemberStateMutex = Arc<RwLock<MemberState>>;
 pub type DatabaseClient = Client;
 
+/// Entrypoint for the rocket application.
 #[rocket::main]
 async fn main() {
     env_logger::init();
@@ -64,21 +64,10 @@ async fn main() {
         "Welcome to OpenKeg {} - The backend of the Musikverein Leopoldsdorf!",
         env!("CARGO_PKG_VERSION")
     );
-    let figment = config::read_config().merge((
-        "ident",
-        Ident::try_new(keg_user_agent()).expect("Valid ident for server response"),
-    ));
-    let member_state = MemberState::mutex();
-    let mut server_result = create_server(figment).manage(member_state);
-    server_result = manage_keys(server_result).attach(Cors);
-    let config = server_result.figment().extract::<Config>().expect("config");
-    server_result = server_result.manage(initialize_client(&config).await);
-    server_result = server_result.manage(ServerInfo::new());
-    server_result = configure_static_directory(server_result);
-    register_user_sync_task(&server_result);
-    match server_result.launch().await {
+    let rocket = configure_rocket(initialize_build_state()).await;
+    match rocket.launch().await {
         Ok(_) => info!("Shutdown OpenKeg!"),
-        Err(err) => error!("failed to start: {}", err.to_string()),
+        Err(err) => error!("Failed to start: {}", err.to_string()),
     }
 }
 
@@ -97,10 +86,44 @@ pub fn keg_user_agent() -> String {
     )
 }
 
-fn create_server(figment: Figment) -> Rocket<Build> {
+/// Create a new [Rocket<Build>] and merge the application configuration into it.
+///
+/// returns: Rocket<Build> the fresh build state
+fn initialize_build_state() -> Rocket<Build> {
+    let figment = config::read_config().merge((
+        "ident",
+        Ident::try_new(keg_user_agent()).expect("Valid ident for server response"),
+    ));
+    rocket::custom(figment).attach(AdHoc::config::<Config>())
+}
+
+/// Compose all the configuration functions to allow a single call to configure the rocket build state.
+/// It is recommended to use this with a fresh [Rocket<Build>] instance.
+///
+/// # Arguments
+///
+/// * `rocket`: the build state which should be configured
+///
+/// returns: Rocket<Build>
+async fn configure_rocket(rocket: Rocket<Build>) -> Rocket<Build> {
+    let configured_rocket = manage_database_client(manage_member_state(manage_keys(attach_cors(
+        manage_server_info(mount_static_directory(mount_controller_routes(rocket))),
+    ))))
+    .await;
+    register_user_sync_task(&configured_rocket);
+    configured_rocket
+}
+
+/// Fetch the routes and OpenApi documentation from the submodules and attach it to the rocket build.
+///
+/// # Arguments
+///
+/// * `rocket`: the build state to attach the routes to
+///
+/// returns: Rocket<Build>
+fn mount_controller_routes(mut rocket: Rocket<Build>) -> Rocket<Build> {
     let openapi_settings = openapi_settings();
     let (info_route, info_spec) = get_info_routes_and_docs(&openapi_settings);
-    let mut rocket = rocket::custom(figment).attach(AdHoc::config::<Config>());
     let mut openapi_spec_header = custom_openapi_spec(&rocket);
     merge_specs(&mut openapi_spec_header, &"".to_string(), &info_spec)
         .expect("OpenApi spec and routes");
@@ -117,19 +140,56 @@ fn create_server(figment: Figment) -> Rocket<Build> {
     rocket.mount("/", get_info_routes_and_docs(&openapi_settings).0.to_vec())
 }
 
-fn register_user_sync_task(server: &Rocket<Build>) {
-    let config: Config = server.figment().extract().expect("config");
-    let member_state_option = server.state::<MemberStateMutex>();
-    if member_state_option.is_none() {
-        warn!("Unable to retrieve member state, scheduled user synchronization will not work");
-        return;
+/// Serve a static directory from the file system.
+/// This is intended to be used for OpenAPI frontends such as [Swagger](https://swagger.io/) or [RapiDoc](https://rapidocweb.com/).
+/// The directory will be served iff [Config::serve_static_directory] is set.
+/// If the directory does not exist on the filesystem while the configuration says it should be served, this function will panic.
+/// When requesting the base of the [Config::static_directory_url], the `index.html` will be returned.
+///
+/// # Arguments
+///
+/// * `rocket`: the state of the application to configure
+///
+/// returns: Rocket<Build> the (configured) application state
+fn mount_static_directory(rocket: Rocket<Build>) -> Rocket<Build> {
+    let config = rocket_configuration(&rocket);
+    if config.serve_static_directory {
+        info!(
+            "Mount static directory '{}' to '{}'",
+            config.static_directory_path, config.static_directory_url
+        );
+        rocket.mount(
+            config.static_directory_url,
+            FileServer::new(
+                config.static_directory_path,
+                Options::Index | Options::NormalizeDirs,
+            ),
+        )
+    } else {
+        rocket
     }
-    let mut member_state_clone = member_state_option
-        .expect("Member state for synchronizing")
-        .clone();
-    task::spawn(async move {
-        member_synchronization_task(&config, &mut member_state_clone).await;
-    });
+}
+
+/// Instantiate a [ServerInfo] and let rocket manage it.
+///
+/// # Arguments
+///
+/// * `rocket`: the current rocket build state
+///
+/// returns: Rocket<Build>
+fn manage_server_info(rocket: Rocket<Build>) -> Rocket<Build> {
+    rocket.manage(ServerInfo::new())
+}
+
+/// Attach the cors fairing to the rocket build state,
+///
+/// # Arguments
+///
+/// * `rocket`: the build state to attach the cors fairing to
+///
+/// returns: Rocket<Build>
+fn attach_cors(rocket: Rocket<Build>) -> Rocket<Build> {
+    rocket.attach(Cors)
 }
 
 /// Let the server manage the private and the public key.
@@ -137,13 +197,13 @@ fn register_user_sync_task(server: &Rocket<Build>) {
 ///
 /// # Arguments
 ///
-/// * `server`: the server where to register the keys
+/// * `rocket`: the server where to register the keys
 ///
 /// returns: Rocket<Build>
-fn manage_keys(server: Rocket<Build>) -> Rocket<Build> {
-    let config: Config = server.figment().extract().expect("config");
+fn manage_keys(rocket: Rocket<Build>) -> Rocket<Build> {
+    let config = rocket_configuration(&rocket);
     info!("Read the public and the private key");
-    let mut server_manage = server;
+    let mut server_manage = rocket;
     match read_private_key(&config) {
         Ok(private_key) => {
             server_manage = server_manage.manage(private_key);
@@ -167,43 +227,85 @@ fn manage_keys(server: Rocket<Build>) -> Rocket<Build> {
     server_manage
 }
 
-/// Serve a static directory from the file system.
-/// This is intended to be used for OpenAPI frontends such as [Swagger](https://swagger.io/) or [RapiDoc](https://rapidocweb.com/).
-/// The directory will be served iff [Config::serve_static_directory] is set.
-/// If the directory does not exist on the filesystem while the configuration says it should be served, this function will panic.
-/// When requesting the base of the [Config::static_directory_url], the `index.html` will be returned.
+/// Create an empty [MemberStateMutex] and let the rocket build state manage it.
+/// This allows the application to provide the member state in the controller calls.
 ///
 /// # Arguments
 ///
-/// * `rocket`: the state of the application to configure
+/// * `rocket`: the build state to attach the [MemberStateMutex] to
 ///
-/// returns: Rocket<Build> the (configured) application state
-fn configure_static_directory(rocket: Rocket<Build>) -> Rocket<Build> {
-    let config: Config = rocket.figment().extract().expect("config");
-    if config.serve_static_directory {
-        info!(
-            "Mount static directory '{}' to '{}'",
-            config.static_directory_path, config.static_directory_url
-        );
-        rocket.mount(
-            config.static_directory_url,
-            FileServer::new(
-                config.static_directory_path,
-                Options::Index | Options::NormalizeDirs,
-            ),
-        )
-    } else {
-        rocket
-    }
+/// returns: Rocket<Build>
+fn manage_member_state(rocket: Rocket<Build>) -> Rocket<Build> {
+    let member_state = MemberState::mutex();
+    rocket.manage(member_state)
 }
 
+/// Initialize the database client and let the rocket build state manage it.
+///
+/// # Arguments
+///
+/// * `rocket`: the build state to let manage the database client
+///
+/// returns: Rocket<Build>
+async fn manage_database_client(rocket: Rocket<Build>) -> Rocket<Build> {
+    let config = &rocket_configuration(&rocket);
+    rocket.manage(initialize_client(config).await)
+}
+
+/// Create a new task which synchronizes the member state with the directory server in the interval given in the [Config].
+/// If there is no [MemberStateMutex] managed by the rocket build state, a warning will be printed and nothing will happen.
+/// This means that [manage_member_state] should be called with the build state first.
+///
+/// # Arguments
+///
+/// * `rocket`: the rocket build state to fetch the [MemberStateMutex] from
+///
+/// returns: ()
+fn register_user_sync_task(rocket: &Rocket<Build>) {
+    let config = rocket_configuration(rocket);
+    let member_state_option = rocket.state::<MemberStateMutex>();
+    if member_state_option.is_none() {
+        warn!("Unable to retrieve member state, scheduled user synchronization will not work. Was 'manage_member_state()' called before?");
+        return;
+    }
+    let mut member_state_clone = member_state_option
+        .expect("Member state for synchronizing")
+        .clone();
+    task::spawn(async move {
+        member_synchronization_task(&config, &mut member_state_clone).await;
+    });
+}
+
+/// Retrieve the configuration from the current rocket build state.
+/// If the configuration cannot be extracted, this function will panic.
+///
+/// # Arguments
+///
+/// * `rocket`: the build state to retrieve the configuration from
+///
+/// returns: Config
+fn rocket_configuration(rocket: &Rocket<Build>) -> Config {
+    rocket.figment().extract().expect("config")
+}
+
+/// Provide the OpenApi settings to be used in this application.
+///
+/// returns: OpenApiSettings
 fn openapi_settings() -> OpenApiSettings {
     Default::default()
 }
 
+/// Create an [OpenApi] structure to use in this application.
+/// This structure will contain the header such as the license, author and server list.
+///
+/// # Arguments
+///
+/// * `rocket`: the build state to retrieve the configuration from
+///
+/// returns: OpenApi
 fn custom_openapi_spec(rocket: &Rocket<Build>) -> OpenApi {
     let rocket_config: rocket::Config = rocket.figment().extract().expect("config");
-    let config: Config = rocket.figment().extract().expect("config");
+    let config = rocket_configuration(rocket);
     use okapi::openapi3::*;
     OpenApi {
         openapi: OpenApi::default_version(),
