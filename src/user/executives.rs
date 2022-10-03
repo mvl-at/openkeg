25f50d6493d66e2ac5a6f672015fa4f81471c402
Richard Stöckl <richard.stoeckl@aon.at>
2022-10-03 14:40:27 +0200

    Add cors options preflight for all urls

--- src/user/executives.rs
+++ src/user/executives.rs
@@ -15,7 +15,6 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-use okapi::openapi3::{Object, SecurityRequirement, SecurityScheme, SecuritySchemeData};
 use rocket::http::Status;
 use rocket::outcome::Outcome::{Failure, Success};
 use rocket::request::{FromRequest, Outcome};
